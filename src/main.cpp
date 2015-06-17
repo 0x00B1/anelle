9140f12614a19ea2757eb3d067554f70d4c0fc2b
Allen Goodman <allen.goodman@icloud.com>
2015-06-17 16:44:11 -0400

    SDL

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,25 @@
 #include "SDL2/SDL.h"
 
 int main(int argc, const char * argv[]) {
-    std::ifstream game_pak(argv[1], std::ios::binary);
+  std::ifstream game_pak(argv[1], std::ios::binary);
 
-    std::cout << game_pak.is_open() << std::endl;
+  std::cout << game_pak.is_open() << std::endl;
+
+  SDL_Surface * surface;
+
+  SDL_Window * window;
+
+  SDL_Init(SDL_INIT_VIDEO);
+
+  window = SDL_CreateWindow("…", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 160, 144, 0);
+
+  surface = SDL_GetWindowSurface(window);
+
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Delay(2000);
+
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
 }
